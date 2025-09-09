ea8b1982ab640207ee2e2c4b55181f3d3226c480
S17-Shwet <nichalshweta77@gmail.com>
2025-09-09 22:30:25 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,40 @@
 // 1st approach is sorting 
 
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.size() != t.size()) return false;
+
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+//         return s==t;
+//     }
+// };
+
+// using frequency count with vector
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+  if(s.size() != t.size()) return false;
+
+  vector<int> freq(26,0);
+
+  for(char c : s)
+  {
+    freq[c-'a']++;
+  }
+  for(char c: t)
+  {
+    freq[c-'a']--;
+  }
+ for(int count: freq)
+ {
+    if(count!=0) return false;
+ }
+ return true;
+
     }
-};
+    };
+
+
