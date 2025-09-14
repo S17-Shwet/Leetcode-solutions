1db4fb9e319c8eb8a5f06400741990993eb1034f
S17-Shwet <nichalshweta77@gmail.com>
2025-09-14 19:19:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (67.51%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,45 +1,17 @@
-// class Solution {
-// public:
-//     bool isPalindrome(int x) {
-//         if(x<0 || (x % 10 == 0 && x!= 0))
-//         return false;
-
-//         int reversedHalf=0;
-//         while(x>reversedHalf)
-//         {
-//             reversedHalf = reversedHalf*10+x%10;
-//             x=x/10;
-//         }
-
-//         //for even digits : x==reversedHalf
-//         //for odd digits : x==reversed/ 10 (middle digit doesn't matter) 
-//         return x == reversedHalf || x == reversedHalf/10;
-//     }
-// };
-
-
-class Solution
-{
-    public:
-
-    bool isPalindrome(int x)
-    {
-
-        long long  revNum=0;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long reversed=0;
         int dup=x;
-
-        while(x>0)
+   
+       if(x<0) return false;
+        while(x!=0)
         {
-            int ld= x % 10;
-
-            revNum = (revNum * 10) + ld;
-
+            int ld=x%10;
+            reversed=reversed*10 + ld;
             x=x/10;
         }
-
-        if(revNum==dup)
-        return true;
-        else
-        return false;
+        return reversed == dup;
+        
     }
 };
