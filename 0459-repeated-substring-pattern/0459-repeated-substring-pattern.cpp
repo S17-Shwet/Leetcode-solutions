1cc316acc0c0f1453f492525e08484dd1dd40ddd
S17-Shwet <nichalshweta77@gmail.com>
2025-09-16 23:56:07 +0530

    Time: 0 ms (100%), Space: 15.2 MB (57.48%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,8 +1,15 @@
+// class Solution {
+// public:
+//     bool repeatedSubstringPattern(string s) {
+//         string doubled=s+s;
+
+//         return doubled.substr(1,doubled.size()-2).find(s) != string::npos;
+//     }
+// };
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         string doubled=s+s;
-
-        return doubled.substr(1,doubled.size()-2).find(s) != string::npos;
+       return doubled.substr(1,doubled.size()-2).find(s) != string :: npos;
     }
 };
