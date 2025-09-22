a869577771934a1023c3466672f5e0550e50cb2f
S17-Shwet <nichalshweta77@gmail.com>
2025-09-23 00:00:15 +0530

    Time: 0 ms (100%), Space: 10.1 MB (25.61%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -7,18 +7,56 @@
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
- */
+//  */
+//  ✅ Approach 1: Count Length + Traverse Again
+
+// Traverse the list once to count nodes (n).
+
+// Middle = n/2 (0-indexed).
+
+// Traverse again n/2 steps and return that node.
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         int length=0;
+        
+//         ListNode* temp=head;
+
+//         while(temp)
+//         {
+//             length++;
+//             temp=temp->next;
+//         }
+//         int mid=length/2;
+//         temp=head;
+
+//         for(int i=0;i<mid;i++)
+//         {
+//             temp=temp->next;
+//         }
+//         return temp;//head return at the end so head starts from the mid
+//     }
+// };
+
+// ✅ Approach 2: Store Nodes in Array / Vector
+
+// Traverse the list and store pointers in an array/vector.
+
+// The middle is at index size/2.
+
+// Return that node.
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
 
-        while(fast!=NULL && fast->next!=NULL)
+        vector<ListNode*> nodes;
+        ListNode* temp=head;
+
+        while(temp)
         {
-            slow=slow->next;
-            fast=fast->next->next;
+            nodes.push_back(temp);
+            temp=temp->next;
         }
-        return slow;
+        return nodes[nodes.size()/2];
     }
 };
