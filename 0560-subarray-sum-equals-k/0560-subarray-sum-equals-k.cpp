1351cbe3e76a1380f097d457a6d4c8b2788f5804
S17-Shwet <nichalshweta77@gmail.com>
2025-10-13 22:14:10 +0530

    Time: 2477 ms (15.08%), Space: 34.4 MB (85.74%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        
-        int count=0;
-        
-       
+     int n=nums.size();
+     int count=0;
 
-        int n = nums.size();
-
-        for(int i=0;i<n;i++)
+     for(int i=0;i<n;i++)
+     {
+        int sum=0;
+        for(int j=i;j<n;j++)
         {
-            int sum=0;
-            for(int j=i;j<=n;j++)
-            {
-                 sum=sum+nums[i];
-                 if(sum==k)
-                 {
-                    count++;
-                 }
-                
-            }
-            
-
+           sum=sum+nums[j];
+           if(sum==k)
+           {
+            count++;
+           }
         }
-        return count;
+     }   
+     return count;
     }
 };
