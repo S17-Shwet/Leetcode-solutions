58812c3b9b49939ca09da4517a02595bee80eb2d
S17-Shwet <nichalshweta77@gmail.com>
2025-10-15 16:37:24 +0530

    Time: 15 ms (38.25%), Space: 14.3 MB (21.57%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,30 +1,60 @@
+// brute force solution
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         int n=s.size();
+
+//         int maxLen=0;
+
+//         for(int i=0;i<n;i++)
+//         {
+//             unordered_set<char>st;
+//             int len=0;
+
+//             for(int j=i;j<n;j++)
+//             {
+//                 if(st.find(s[j]) != st.end())
+//                 {
+//                     //if char is found in string
+//                     break;//no need duplicate
+//                 }
+//                 //if not found then 
+//                 st.insert(s[j]);
+//                 len++;
+//             }
+
+//             maxLen=max(maxLen,len);
+//         }
+//         return maxLen;
+        
+//     }
+// };
+
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.size();
 
-        int maxLen=0;
+        unordered_set<char>st;
 
-        for(int i=0;i<n;i++)
+        int n=s.size(), maxLen=0, start=0,end=0;
+        while(end < n)
         {
-            unordered_set<char>st;
-            int len=0;
-
-            for(int j=i;j<n;j++)
+            if(st.find(s[end]) == st.end())
             {
-                if(st.find(s[j]) != st.end())
-                {
-                    //if char is found in string
-                    break;//no need duplicate
-                }
-                //if not found then 
-                st.insert(s[j]);
-                len++;
+               st.insert(s[end]);
+               maxLen=max(maxLen,end-start+1);
+               end++;
+             
+            }
+            else
+            {
+                st.erase(s[start]);
+                start++;
             }
 
-            maxLen=max(maxLen,len);
         }
-        return maxLen;
-        
+
+ return maxLen;
     }
 };
