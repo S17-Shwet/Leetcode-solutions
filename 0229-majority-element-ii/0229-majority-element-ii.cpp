9f51cf17415a72835b4d013e12ade5ba30d890d8
S17-Shwet <nichalshweta77@gmail.com>
2025-10-20 23:53:35 +0530

    Time: 0 ms (100%), Space: 23.5 MB (81.51%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -30,28 +30,67 @@
 // };
 
 // using hash map 2nd method
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//      unordered_map<int,int>freq;
+//      int n=nums.size();
+//      vector<int> result;
+
+//     //  count freq of each element in array
+//     for(int num:nums)
+//     {
+//         freq[num]++;
+//     }
+//     // catch up all eles with freq > n/3
+//     for(auto &p: freq)
+//     //"auto Figure out the type automatically for me."
+//     // for (pair<const int,int> &p : freq) instead of this write auto
+//     // p refers to the actual element in freq
+//     {
+//         if(p.second > n/3)
+//         {
+//             result.push_back(p.first);
+//         }
+
+//     }
+//     return result;
+
+//     }
+// };
+
+// efficient solution Boyer-voting algorithm
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-     unordered_map<int,int>freq;
-     int n=nums.size();
-     vector<int> result;
+        
+        int n=nums.size();
 
-    //  count freq of each element in array
-    for(int num:nums)
-    {
-        freq[num]++;
-    }
-    // catch up all eles with freq > n/3
-    for(auto &p: freq)
-    {
-        if(p.second > n/3)
+        int cand1=0,cand2=1;
+        int count1=0,count2=0;
+
+        for(int num: nums)
         {
-            result.push_back(p.first);
+            if(num == cand1) count1++;
+            else if(num == cand2) count2++;
+
+            else if(count1==0) {cand1=num; count1=1;}
+            else if(count2 == 0) {cand2=num; count2=1;}
+            else{count1--; count2--;}
+
+
         }
 
-    }
-    return result;
-
+        // verify counts
+        count1=count2=0;
+        for(int num: nums)
+        {
+            if(num == cand1) count1++;
+            else if(num == cand2) count2++;
+        }
+        vector<int> res;
+        if(count1 > n/3) res.push_back(cand1);
+        if(count2 > n/3) res.push_back(cand2);
+        return res;
     }
 };
