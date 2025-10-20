9078c6a769157f4fa7c7b9304748d461ae10728c
S17-Shwet <nichalshweta77@gmail.com>
2025-10-20 23:07:13 +0530

    Time: 9 ms (26.87%), Space: 26.4 MB (29.15%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,22 +1,57 @@
+ //brute force solution
+// class Solution {
+// public:
+//     vector<int> majorityElement(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> res;
+
+//         for(int i=0;i<n;i++)
+//         {
+//             if(find(res.begin(),res.end(),nums[i]) != res.end())
+//             continue;//skip if already added
+//             // [3,2,3]
+//             // as 3 is counted 2 times and added to vector
+//             // so if found in res just skip counting it
+//             // so prevent ans like [3,3]
+
+//             int count=0;
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(nums[i]==nums[j]) count++;
+//                 // for nums[i]=3 
+//                 // for num[j] when 2 not matches
+//                 // only 3 matches so again count is 2
+//             }
+//             if(count > n/3)
+//             res.push_back(nums[i]);
+//         }
+//         return res;
+//     }
+// };
+
+// using hash map 2nd method
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> res;
+     unordered_map<int,int>freq;
+     int n=nums.size();
+     vector<int> result;
 
-        for(int i=0;i<n;i++)
+    //  count freq of each element in array
+    for(int num:nums)
+    {
+        freq[num]++;
+    }
+    // catch up all eles with freq > n/3
+    for(auto &p: freq)
+    {
+        if(p.second > n/3)
         {
-            if(find(res.begin(),res.end(),nums[i]) != res.end())
-            continue;//skip if already added
-
-            int count=0;
-            for(int j=0;j<n;j++)
-            {
-                if(nums[i]==nums[j]) count++;
-            }
-            if(count > n/3)
-            res.push_back(nums[i]);
+            result.push_back(p.first);
         }
-        return res;
+
+    }
+    return result;
+
     }
 };
