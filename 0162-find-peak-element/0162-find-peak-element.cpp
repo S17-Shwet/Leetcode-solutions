3555610d7b789344d5ad2a7b5751d4d5a6f982dc
S17-Shwet <nichalshweta77@gmail.com>
2025-11-18 23:45:23 +0530

    Time: 0 ms (100%), Space: 12.4 MB (70.72%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,20 +1,44 @@
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+//         int n=nums.size();
+
+//         int max=nums[0];
+//         int maxIndex=0;
+       
+//         for(int i=1;i<n;i++)
+//         {
+//             if(nums[i]>max)
+//             {
+//                 max=nums[i];
+//                 maxIndex=i;
+
+//             }
+//         }
+//         return maxIndex;
+//     }
+// };
+
+
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-
-        int max=nums[0];
-        int maxIndex=0;
-       
-        for(int i=1;i<n;i++)
+    int findPeakElement(vector<int>& arr) {
+ int n=arr.size();
+        int low=0;int high=n-1;
+        while(low<high)
         {
-            if(nums[i]>max)
+            int mid=low+(high-low)/2;
+            
+            
+            if(arr[mid] < arr[mid+1])
             {
-                max=nums[i];
-                maxIndex=i;
-
+                low=mid+1;
+            }
+            else
+            {
+                high=mid;
             }
         }
-        return maxIndex;
+        return low; //low==high
     }
-};
+        };
