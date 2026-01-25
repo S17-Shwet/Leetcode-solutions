8474dccadc2e0d6d0994fabdeebdddc6538605aa
Shweta Nilkanth Nichal <nichalshweta77@gmail.com>
2026-01-25 23:27:17 +0530

    Time: 11 ms (34.81%), Space: 12.4 MB (54.54%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -12,6 +12,11 @@ public:
 
         node->val=node->next->val;//copy val from next value
         node->next=node->next->next;//bypass the next node
-
+// if(head->val == node->val)
+// {
+//     Node* temp=head->val;
+//     head=head->next;
+//     delete temp;
+// }
     }
 };
